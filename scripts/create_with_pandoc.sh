b749f8e657c3ea0c55b27d394f68285611929a5a
Louis Vela <lvela@enfocussolutions.com>
2013-11-23 15:45:36 -0600

    Fixed to work in scripts dir.

--- scripts/create_with_pandoc.sh
+++ scripts/create_with_pandoc.sh
@@ -1,15 +1,15 @@
 pandoc -S --epub-metadata=metadata.xml -o dont_know_js.epub \
-preface.md \
-./scope\ \&\ closures/toc.md \
-./scope\ \&\ closures/ch1.md \
-./scope\ \&\ closures/ch2.md \
-./scope\ \&\ closures/ch3.md \
-./scope\ \&\ closures/ch4.md \
-./scope\ \&\ closures/ch5.md \
-./scope\ \&\ closures/apA.md \
-./scope\ \&\ closures/apB.md \
-./scope\ \&\ closures/apC.md \
-./scope\ \&\ closures/apD.md \
-./this\ \&\ object\ prototypes/toc.md \
-./this\ \&\ object\ prototypes/ch1.md \
-./this\ \&\ object\ prototypes/ch2.md 
+../preface.md \
+../scope\ \&\ closures/toc.md \
+../scope\ \&\ closures/ch1.md \
+../scope\ \&\ closures/ch2.md \
+../scope\ \&\ closures/ch3.md \
+../scope\ \&\ closures/ch4.md \
+../scope\ \&\ closures/ch5.md \
+../scope\ \&\ closures/apA.md \
+../scope\ \&\ closures/apB.md \
+../scope\ \&\ closures/apC.md \
+../scope\ \&\ closures/apD.md \
+../this\ \&\ object\ prototypes/toc.md \
+../this\ \&\ object\ prototypes/ch1.md \
+../this\ \&\ object\ prototypes/ch2.md 
